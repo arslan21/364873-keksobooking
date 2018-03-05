b4fc7417ff6954ed612a665a4ece08862222177d
Arslan <rauros@list.ru>
2018-03-05 12:54:08 +0300

    refactored drag.js

--- js/map.js
+++ js/map.js
@@ -9,9 +9,54 @@
     top: 375
   };
 
+  var dragablePin = document.querySelector('.map__pin--main');
+  var startCoords = {};
+  var diff = {};
+
+  function getCoords(evt) {
+    startCoords = {
+      x: evt.pageX,
+      y: evt.pageY,
+    };
+
+    diff = {
+      x: startCoords.x - dragablePin.offsetLeft,
+      y: startCoords.y - dragablePin.offsetTop
+    };
+  }
+
+  function onMouseMove(moveEvt) {
+    if (moveEvt.pageX - diff.x > 0 && moveEvt.pageX - diff.x < 1200 && moveEvt.pageY - diff.y > 150 && moveEvt.pageY - diff.y < 500) {
+      dragablePin.style.left = (moveEvt.pageX - diff.x) + 'px';
+      dragablePin.style.top = (moveEvt.pageY - diff.y) + 'px';
+    } else {
+      document.addEventListener('mouseup', onMouseUp);
+    }
+  }
+
+  function onMouseUp(upEvt) {
+    upEvt.preventDefault();
+    window.form.setAddress();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    dragablePin.removeEventListener('mouseup', onMouseUp);
+  }
+
+  function dragPin () {
+    dragablePin.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
+      getCoords(evt);
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+      dragablePin.addEventListener('mouseup', onMouseUp);
+    });
+  }
+
   window.map = {
     initialize: function () {
-      window.drag.dragPin();
+      dragPin();
       mapPinMain.addEventListener('mouseup', function (evt) {
         map.classList.remove('map--faded');
         window.backend.load(window.filter.loadData, window.errorMessage.show);
