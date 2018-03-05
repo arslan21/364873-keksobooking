7d1f28de3dc6259d2bff0756b91749ed13456d4b
Arslan <rauros@list.ru>
2018-03-05 11:49:39 +0300

    Revert "refactored data.js"
    This reverts commit ce3d398d52ee27de14a48a9cd34e4ee6d052530e.

---
+++ js/data.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+
+  //  этот файл используется для вспомогательных данных
+
+  window.data = {
+    OFFER_TYPES: {
+      flat: {
+        name: 'Квартира',
+        minPrice: 1000,
+      },
+      bungalo: {
+        name: 'Бунгало',
+        minPrice: 0,
+      },
+      house: {
+        name: 'Дом',
+        minPrice: 5000,
+      },
+      palace: {
+        name: 'Дворец',
+        minPrice: 10000,
+      }
+    },
+
+    pictures: [
+      'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+      'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+      'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+    ]
+
+  };
+
+})();
